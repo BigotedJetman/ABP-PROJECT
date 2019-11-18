05f2c5cc18e00b622ea84026a7662f092d0faf40
ism46999937 <abailonescobero@gmail.com>
2019-11-18 20:40:27 +0100

    Add files via upload

---
+++ PROYECTO ABP/MENU/js/javascript.js
@@ -0,0 +1,25 @@
+function volver_inicio(){
+    location.href="../inicio/inicio.html"
+    var inici = location.href
+    return inici
+}
+function explicacion(){
+    location.href="../explicacion/explicacion.html"
+    var expli = location.href
+    return expli
+}
+function ver_mapa(){
+    location.href="../mapa/mapa.html"
+    var map = location.href
+    return map
+}
+function juegos_eleccion(){
+    location.href="../juegos/juegos_eleccion.html"
+    var juegos = location.href
+    return juegos
+}
+function ver_ranking(){
+    location.href="../ranking/ranking.html"
+    var rank = location.href
+    return rank
+}
